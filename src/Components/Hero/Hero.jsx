e06709f9979fe72fcfbf0d5095e739af659fa644
Manmohan Suthar <135035572+ManmohanSuthar@users.noreply.github.com>
2023-12-25 21:32:57 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
         <div className="hero_right">
           <div className="hero_photo">
-            <img src="https://lh3.googleusercontent.com/pw/ABLVV85Xwgg9sHXv6ndhq24P8drBQI-9j-CXFuh6H0i91iiFeeIVnXxFDwJLN0WPqSpv0xP5HqnZ--Jponz4ZGn0eSaFwKC2C2siwcJLdMmcKNf41iIV1eNv3bjuQ4GXbq0UxN6u20VH5_0qc-_emav_OuVCmvya7xAAZthoMQHuG8xVIsjM4DptZtbsV_3DfINafc-wc09yXfAJd7rygAWxMPbHwn61xTezBxc0bbRF9OOppD7QJDh8RQmOApAFHSXc0JE7EKGjx75Cq60n-GLGWB_b1hG0JEutCPV9CbIZ5nano1DuigDFmjcmTek5QjSiIjREYmw2crDQy-gt_86_bgefi--f6_g-rfIidam14dDF6ApnwqcKm2YQ3HexIb-zGo3ZGViE6-B-REh_fLfr3CpgYNXpdmHDGRGgEFTDuKLZaBl7HEhRYTDY8DxzbLE-lNnWrvz-7tVJlehmssyu0AkwHsw-qDTCcnELsGlzlLqWcnVNMrRA_hctD5rUUtefKt3FncI3jV9PyHnukvTOs8U9XD1lwRD56Pd6Ct819-sg0ypLmP1zaj6g0MLNbuvAIwKGuB6G8uC4dRoU-EGQS8r6ijibSOI-mCW_1LAQAFlfn7mRCiGaUp4KmLaf3hn-_XXODClNlH1vxWhvM0DXG4Ewn4SHyL6eSlPqqkQXNAfz7WhSbxyfaZWfRvQE2tk2BYk9j81lOvtgdPrs4WcSKCuQx_CgzurJLjtDXJuhGPOhjrgSVDAB62m2PnGn5LViiesvKexqsKkbx_cgMblmCueTthacA14Nrw7mtwWzm34PBzXAqZa0ooyUzaeVSVcKn3DjSJAsOx8dC1rJml9ADGBV61TgZCX2g7MrCMv64z_mqEWKk_1d-jIJQ5-9fMjxsg=w487-h487-s-no-gm?authuser=0" />
+            <img src="https://media-del1-1.cdn.whatsapp.net/v/t61.24694-24/369620603_585695180289481_8194852975439612399_n.jpg?ccb=11-4&oh=01_AdSgOhPxq_bLHvPZukQwiqcbXdPcCFT5UQ4tX263fWbenQ&oe=6596A4F3&_nc_sid=e6ed6c&_nc_cat=106" />
           </div>
         </div>
       </div>
